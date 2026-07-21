de08e6ce10ed75d757002b9b52d571798d488b93
HALI12_hat <hussainaliabdulrasool004@gmail.com>
2026-07-22 00:36:05 +0300

    Made new function in profile controller that fetches comments iof the current user from every challenge theyve commented on

--- controllers/profile.js
+++ controllers/profile.js
@@ -2,10 +2,22 @@ const User = require('../models/user')
 const Challenge = require('../models/challenge')
 
 const showProfile = async (req, res)=>{
-  const currentUser =  await User.findById(req.session.user._id)
+    const currentUser =  await User.findById(req.session.user._id)
+
     const userChallenges = await Challenge.find({owner: currentUser._id})
 
-    res.render('profile.ejs', {currentUser, userChallenges})
+    const challengesWithUserComments = await Challenge.find({'comments.author': currentUser._id})
+
+    const userComments = []
+    challengesWithUserComments.forEach(function(challenge){
+        challenge.comments.forEach(function(comment){
+            if(comment.author && comment.author.equals(currentUser._id)){
+                userComments.push({comment: comment, challenge: challenge})
+            }
+        })
+    })
+
+    res.render('profile.ejs', {currentUser, userChallenges, userComments})
 }
 
 module.exports = 
